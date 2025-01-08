912ff566106d764762a9340bfeba317ef21286c7
Diego Materano <153313506+diegogo69@users.noreply.github.com>
2025-01-07 20:27:41 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// .eslintrc.js example
+module.exports = {
+  "env": {
+      "browser": true,
+      "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "airbnb-base",
+    "prettier"
+  ],
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+}
