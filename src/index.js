23234b8375c86e1006c756e497599a880963593a
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-01-09 21:56:25 -0400

    FEAT: Use temp localStorage weather

--- src/index.js
+++ src/index.js
@@ -10,17 +10,23 @@ import { fetchWeather } from "./static/js/fetch_weather";
 import { parseWeather } from "./static/js/parseWeather";
 
 import { domInit } from "./static/js/dom_init";
+import { domUpdate } from "./static/js/dom_update";
 
 async function main() {
-    const weather = await fetchWeather('caracas');
-    const parsed = parseWeather(weather);
-    console.log(parsed);
+    if (!(localStorage.getItem('weather'))) {
+        const weather = await fetchWeather('caracas');
+        const parsed = parseWeather(weather);
+        console.log(parsed);
+        localStorage.setItem('weather', JSON.stringify(parsed));
+    }
+
+    domUpdate(JSON.parse(localStorage.getItem('weather')));
         
 }
 
-
-domInit()
-// main()
+// localStorage.clear();
+domInit();
+main();
 // Setup a form for user input and request weather of location
 
 
