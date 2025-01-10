b4b433fc6e895c9e806d3011767fd6dc109ec603
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-01-10 14:40:51 -0400

    FEAT: Fetch next 7 days

--- src/static/js/fetch_weather.js
+++ src/static/js/fetch_weather.js
@@ -2,14 +2,11 @@ export { fetchWeather };
 import { format } from "date-fns";
 import { addDays } from "date-fns";
 
-// Format summary date
-// 'EEEE MMM d(o)' Wednesday Jan 8
-
-// Get weather from location
+const NEXT_DAYS = 7;
 
 const API_KEY = 'JEPSLX6ZDSGFY6GFXGTRNWS9J';
 const date1 = format(new Date(), 'yyyy-MM-dd');
-const date2 = format(addDays(new Date(), 5), 'yyyy-MM-dd');
+const date2 = format(addDays(new Date(), NEXT_DAYS), 'yyyy-MM-dd');
 
 console.log(date1);
 console.log(date2);
@@ -17,6 +14,7 @@ console.log(date2);
 async function fetchWeather(location) {
     console.log('fetching weather');
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?key=${API_KEY}`;
+    console.log(url);   
 
     const fetchResponse = await fetch(url, {mode: 'cors'});
     return await fetchResponse.json();
