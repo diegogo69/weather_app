730677ffcd95eee467b09e4e2323e302eb06b1e7
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-01-10 21:47:35 -0400

    FEAT: Modularize details info\n\nDetails info being icon name and measure unit

---
+++ src/static/js/details_info.js
@@ -0,0 +1,40 @@
+export { detailsInfo }
+const detailsInfo = {
+    tempmin: {
+        name: 'tempmin',
+        class: 'tempmin',
+        icon: 'thermometer-exterior',
+        unit: '°F'
+    },
+    tempmax: {
+        name: 'tempmax',
+        class: 'tempmax',
+        icon: 'thermometer',
+        unit: '°F'
+    },
+    humidity: {
+        name: 'humidity',
+        class: 'humidity',
+        icon: 'humidity',
+        unit: '%'
+    },
+    // moonphase,
+    sunrise: {
+        name: 'sunrise',
+        class: 'sunrise',
+        icon: 'sunrise',
+        unit: 'hour'
+    },
+    sunset: {
+        name: 'sunset',
+        class: 'sunset',
+        icon: 'sunset',
+        unit: 'hour'
+    },
+    windspeed: {
+        name: 'windspeed',
+        class: 'windspeed',
+        icon: 'strong-wind',
+        unit: 'mph'
+    }
+}
