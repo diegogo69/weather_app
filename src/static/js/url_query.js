caca08492375a1e60ff0a0dd0180e4179d776fc7
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-01-11 21:02:22 -0400

    FEAT: url stands on app data
    URL creation stands on internal app data values
    For location, unitgroup and language
    Therefore it works with no arguments at invoke

--- src/static/js/url_query.js
+++ src/static/js/url_query.js
@@ -1,10 +1,11 @@
 export { urlQuery }
 
 import { API_KEY, DATE1, DATE2 } from "./app_values";
+import { appValues } from "./app_values";
 
 // https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=[YOUR_API_KEY]&include=[days]&unitGroup=[us/metric]&lang[en/es]
 
-function urlQuery ( { location='london', unitGroup='us', lang='en', YOUR_API_KEY=API_KEY, date1=DATE1, date2=DATE2, }) {
+function urlQuery(location = appValues.getLocation(), unitGroup = appValues.getUnitGroup(), lang = appValues.getLang(), YOUR_API_KEY = API_KEY, date1 = DATE1, date2 = DATE2) {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?key=${YOUR_API_KEY}&include=days&unitGroup=${unitGroup}&lang=${lang}`;
     console.log(url);
     return url;
