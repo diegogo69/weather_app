782be43536c4c959c938d740523b662118d0015d
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-01-11 22:01:56 -0400

    REFAC: Values and exports

--- src/static/js/app_values.js
+++ src/static/js/app_values.js
@@ -1,5 +1,3 @@
-export { WEEK_DAYS}
-export { API_KEY, DATE1, DATE2 }
 export { appValues }
 
 import { format, addDays } from "date-fns";
@@ -10,8 +8,13 @@ const appValues = (function () {
     const DATE1 = format(new Date(), 'yyyy-MM-dd');
     const DATE2 = format(addDays(new Date(), WEEK_DAYS), 'yyyy-MM-dd');
 
-    const METRIC = 'Km/h - °C';
-    const US = 'Mph - °F';
+    const units = {
+        metric: 'km/h - °C',
+        us: 'mph - °F',
+        temp: '°F',
+        speed: 'mph',
+        percent: '%'
+    }
 
     const ES = 'Español';
     const EN = 'English';
@@ -46,20 +49,24 @@ const appValues = (function () {
 
     function changeUnitGroup() {
         unitGroup = (unitGroup === 'us') ? 'metric' : 'us';
+
+        switch (unitGroup) {
+            case 'us':
+                units.temp = '°F';
+                units.speed = 'mph';
+                break;
+            case 'metric':
+                units.temp = '°C';
+                units.speed = 'km/h';
+                break;
+        }
     }
 
     return {
         WEEK_DAYS, API_KEY, DATE1, DATE2,
-        METRIC, US, ES, EN,
+        ES, EN, units,
         getLang, changeLang,
         getUnitGroup, changeUnitGroup,
-        getLocation, setLocation
+        getLocation, setLocation,
     }
-})()
-
-const {
-    WEEK_DAYS,
-    API_KEY,
-    DATE1,
-    DATE2
-} = appValues;
+})();
